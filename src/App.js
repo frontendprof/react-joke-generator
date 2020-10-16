ba78bb0143cc386aa38996007f4444b86c086bae
frontendprof <abdumaliksharipov@gmail.com>
2020-10-16 20:30:13 +0500

    Initial setup of joke gen app

--- src/App.js
+++ src/App.js
@@ -1,26 +1,20 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+
+
+
+const API_URL="http://api.icndb.com/jokes/random";
+
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Chuck Norris Jokes Generators</h1>
+      <h2>Here goes a joke:</h2>
+      <button>Get a new joke 😄 </button>
     </div>
   );
 }
 
 export default App;
+
